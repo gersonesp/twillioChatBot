3b63751fd84b8488b94affa982c4aef3d7a2db1d
Gerson Espinal <gerson.espinal@gmail.com>
2019-07-29 19:12:47 -0400

    Twilio api setup

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const tokenRouter = require("./routes/token");
 
 const app = express();
 const port = 1337;
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/token", tokenRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
